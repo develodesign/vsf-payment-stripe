aecfa339f0c63861f3d72b7b0d5f3183eda5c61d
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2019-01-13 16:12:22 +0400

    Payment method registration.

--- hooks/beforeRegistration.ts
+++ hooks/beforeRegistration.ts
@@ -1,4 +1,25 @@
 // This function will be fired both on server and client side context before registering other parts of the module
 export function beforeRegistration(Vue, config, store, isServer) {
-  if (!Vue.prototype.$isServer) console.info('This will be called before extension registration and only on client side')
+  const VSF_PAYMENT_CODE = 'stripe'
+
+  store.dispatch('payment/addMethod', {
+    'title': 'Stripe',
+    'code': VSF_PAYMENT_CODE,
+    'cost': 0,
+    'costInclTax': 0,
+    'default': false,
+    'offline': false
+  })
+
+  if (!Vue.prototype.$isServer) {
+
+    store.watch((state) => state.checkout.paymentDetails, (prevMethodCode, newMethodCode) => {
+      let method = (newMethodCode === VSF_PAYMENT_CODE) ? '$on' : '$off'
+      // Register/unregister the handler for what happens when they click the place order button.
+      Vue.prototype.$bus[method]('checkout-before-placeOrder', () => {
+          Vue.prototype.$bus.$emit('checkout-do-placeOrder', {})
+      })
+    })
+
+  }
 }
